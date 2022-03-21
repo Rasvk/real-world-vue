640f70e233aa1f7fa99545be28b96718467840b4
Rasvk <rasmusvedholmkrog@gmail.com>
2022-03-21 09:18:45 +0100

    formFieldMixin working

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -19,24 +19,16 @@
 </template>
 
 <script>
+import { formFieldMixin } from '@/mixins/formFieldMixins'
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
     options: {
       type: Array,
       required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    value: [String, Number]
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   },
+
   computed: {
     listeners() {
       return {
